ef8bfaa14f84b930f7a64865535028c82a1ecc52
Luis SEVERINO <lfs.severino@gmail.com>
2016-01-06 02:32:36 +0100

    tmp symlinks to use Bigfoot Core and Content Bundle at src and add and validate their dependencies one by one; Add ContentBundle to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bigfoot\Bundle\CoreBundle\BigfootCoreBundle(),
+            new Bigfoot\Bundle\ContentBundle\BigfootContentBundle(),
             new AppBundle\AppBundle(),
         ];
 
