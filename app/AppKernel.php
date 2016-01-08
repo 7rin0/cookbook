607bf14a2fb498636ed352790c0b8cee9eb158b1
Luis SEVERINO <lfs.severino@gmail.com>
2016-01-08 03:47:59 +0100

    Add BigfootDefaultThemeBundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,7 +50,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'admin_dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
