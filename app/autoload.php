6e567cd5854f5892825c4eb4f7fead5beee11e28
Luis SEVERINO <lfs.severino@gmail.com>
2016-01-09 18:10:19 +0100

    Use Symfony class loader to register classes

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,18 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+// Use Symfony class loader to register classes
+$loader->add('Bigfoot\\Bundle\\CoreBundle', __DIR__ . '/../bigfoot/core-bundle');
+$loader->add('Bigfoot\\Theme\\DefaultThemeBundle', __DIR__ . '/../bigfoot/theme-bundle');
+$loader->add('Bigfoot\\Bundle\\CoreBundle', __DIR__ . '/../bigfoot/core-bundle');
+$loader->add('Bigfoot\\Bundle\\CoreBundle', __DIR__ . '/../bigfoot/core-bundle');
+$loader->add('Bigfoot\\Bundle\\CoreBundle', __DIR__ . '/../bigfoot/core-bundle');
+$loader->add('Bigfoot\\Bundle\\UserBundle', __DIR__ . '/../bigfoot/user-bundle');
+$loader->add('Bigfoot\\Bundle\\CoreBundle', __DIR__ . '/../bigfoot/core-bundle');
+$loader->add('Bigfoot\\Bundle\\ContextBundle', __DIR__ . '/../bigfoot/context-bundle');
+$loader->add('Bigfoot\\Bundle\\ContentBundle', __DIR__ . '/../bigfoot/content-bundle');
+$loader->add('BeSimple\\I18nRoutingBundle', __DIR__ . '/../besimple/i18n-routing-bundle');
+
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
