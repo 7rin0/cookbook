369f3dfcc4d1bb34b6e4955e5406be4d28ddce00
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-11 22:32:01 +0200

    connecting the dots

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// Thanks to http://code.tutsplus.com/tutorials/real-time-chat-with-nodejs-socketio-and-expressjs--net-31708
+
+// Create application
 var express = require("express");
 var app = express();
 var port = 3700;
- 
+
+// Define route
+// app.get("/", function(req, res){
+//     res.send("It works!");
+// });
+
+app.set('views', __dirname + '/src/tpl');
+app.set('view engine', "pug");
+app.engine('pug', require('pug').__express);
 app.get("/", function(req, res){
-    res.send("It works!");
+    res.render("index");
 });
- 
-app.listen(port);
+
+// Run Server
+app.use(express.static(__dirname + '/web'));
+var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
