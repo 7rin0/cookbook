52dc5232ef5d0335e54ce8bf20529620a1429dc3
Luis SEVERINO <lfs.severino@gmail.com>
2016-06-29 23:56:56 +0200

    add ldap, mail, navigation, config and dev-tools

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Navigation',
+    'Zend\Mail',
     'Zend\InputFilter',
     'Zend\Filter',
     'Zend\Hydrator',
