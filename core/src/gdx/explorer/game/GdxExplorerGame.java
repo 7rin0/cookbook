c37ea1476d804574c7e166ed0d3bd5d85fc3f394
Luis SEVERINO <lfs.severino@gmail.com>
2016-07-30 19:18:42 +0200

    Adding the Bucket

--- core/src/gdx/explorer/game/GdxExplorerGame.java
+++ core/src/gdx/explorer/game/GdxExplorerGame.java
@@ -3,17 +3,54 @@ package gdx.explorer.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GdxExplorerGame extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		// load images for bucket and droplet
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// load sounds
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// start music
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// create camera
+		// true as first parameter will set the
+		// y-axis to point downwards.
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 400);
+
+		// create batch
+		batch = new SpriteBatch();
+
+		// Set bucket dimensions
+		bucket = new Rectangle();
+		bucket.width = 64;
+		bucket.height = 64;
+		bucket.x = 800 / 2 - bucket.width / 2;
+		bucket.y = 20;
 	}
 
 	@Override
