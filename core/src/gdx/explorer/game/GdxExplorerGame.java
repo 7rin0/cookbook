6a9aac01016781f4775dea9c99d2512b7686fe10
Luis SEVERINO <lfs.severino@gmail.com>
2016-07-30 22:04:04 +0200

    test libgdx

--- core/src/gdx/explorer/game/GdxExplorerGame.java
+++ core/src/gdx/explorer/game/GdxExplorerGame.java
@@ -2,14 +2,21 @@ package gdx.explorer.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class GdxExplorerGame extends ApplicationAdapter {
 	Texture img;
@@ -20,6 +27,8 @@ public class GdxExplorerGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	
 	@Override
 	public void create () {
@@ -42,8 +51,6 @@ public class GdxExplorerGame extends ApplicationAdapter {
 		// y-axis to point downwards.
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 400);
-
-		// create batch
 		batch = new SpriteBatch();
 
 		// Set bucket dimensions
@@ -52,6 +59,10 @@ public class GdxExplorerGame extends ApplicationAdapter {
 		bucket.height = 64;
 		bucket.x = 800 / 2 - bucket.width / 2;
 		bucket.y = 20;
+
+		// Create rain drops
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
 	}
 
 	@Override
@@ -59,9 +70,16 @@ public class GdxExplorerGame extends ApplicationAdapter {
 		// draw the bucket
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// tell the camera to update its matrices.
+		camera.update();
 		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
 
 		// Making the Bucket Move (Mouse/Touch)
@@ -79,11 +97,43 @@ public class GdxExplorerGame extends ApplicationAdapter {
 		// We also need to make sure our bucket stays within the screen limits
 		if(bucket.x < 0) bucket.x = 0;
 		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		// We also need to make our raindrops move,
+		// let's take the easy route and have them
+		// move at a constant speed of 200 pixels/units per second.
+		// If the raindrop is beneath the bottom edge of the screen,
+		// we remove it from the array.
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while(iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
 	}
 	
 	@Override
 	public void dispose () {
+		// dispose of all the native resources
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
 		batch.dispose();
-		img.dispose();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 }
