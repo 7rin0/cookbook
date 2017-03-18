5d557391bdb232f716c45a54f9c990f9c21fdfa8
Luis SEVERINO <lfs.severino@gmail.com>
2017-03-18 15:15:27 +0100

    Update structure

---
+++ SF3/app/AppKernel.php
@@ -0,0 +1,102 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class AppKernel
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * @return array
+     */
+    public function registerBundles()
+    {
+        $bundles = [
+            // Symfony Bundles
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // Doctrine Bundles
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+
+            // KNP Bundles
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // Other Bundles
+            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
+            new BeSimple\I18nRoutingBundle\BeSimpleI18nRoutingBundle(),
+            new Craue\FormFlowBundle\CraueFormFlowBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new JMS\TwigJsBundle\JMSTwigJsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+//            new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            #new Presta\SitemapBundle\PrestaSitemapBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+
+            // Bigfoot Bundles
+            new Bigfoot\Bundle\CoreBundle\BigfootCoreBundle(),
+            new Bigfoot\Bundle\ContextBundle\BigfootContextBundle(),
+            new Bigfoot\Bundle\NavigationBundle\BigfootNavigationBundle(),
+            new Bigfoot\Bundle\UserBundle\BigfootUserBundle(),
+            new Bigfoot\Bundle\ContentBundle\BigfootContentBundle(),
+            new Bigfoot\Theme\DefaultThemeBundle\BigfootDefaultThemeBundle(),
+            new Bigfoot\Bundle\MediaBundle\BigfootMediaBundle(),
+
+            // Application Bundle
+            new AppBundle\AppBundle(),
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'admin_dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return dirname(__DIR__) . '/var/logs';
+    }
+
+    /**
+     * @param LoaderInterface $loader
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+}
